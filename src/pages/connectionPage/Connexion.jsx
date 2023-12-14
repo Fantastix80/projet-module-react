0121ed9b19f3490a7452e02040b69cd4a9057def
BrasSelva <147380508+BrasSelva@users.noreply.github.com>
2023-12-14 10:15:53 +0100

    Update Connexion.jsx

--- src/pages/connectionPage/Connexion.jsx
+++ src/pages/connectionPage/Connexion.jsx
@@ -2,9 +2,31 @@
 const Connexion = () => {
     return (
         <>
-            <p>Le formulaire de connexion</p>
-        </>
-    );
+            <div className="Connexion">
+            <h1>Connexion</h1>
+        <form>
+        <input
+          type="text"
+          name="email"
+          placeholder="Entrez votre email"
+        />
+        <br/>
+        <div> 
+          <input
+            type="text"
+            name="password"
+            placeholder="Entrez votre mdp"
+          />
+        <button type="submit">
+          Valider
+        </button>
+        </div>
+      </form>
+    </div>
+    
+    
+    </>
+  );
 };
-
+        
 export default Connexion;
