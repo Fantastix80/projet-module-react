466d5bb54e784bd325da64f6f61c15ea2fa57072
BrasSelva <147380508+BrasSelva@users.noreply.github.com>
2023-12-14 12:01:00 +0100

    Update Inscription.jsx

--- src/pages/connectionPage/Inscription.jsx
+++ src/pages/connectionPage/Inscription.jsx
@@ -1,37 +1,42 @@
+import '../../assets/css/inscription.css'
+
 
 const Inscription = () => {
     return (
         <>
-            <div className="Connexion">
-            <h1>Inscription</h1>
-        <form>
-        <input
-          type="text"
-          name="Pseudonyme"
-          placeholder="Entrez votre Pseudonyme"
-        />
-        <br/>
-        <input
-          type="text"
-          name="email"
-          placeholder="Entrez votre email"
-        />
-        <div> 
-          <input
-            type="text"
-            name="password"
-            placeholder="Entrez votre mdp"
-          />
-        <button type="submit">
-          Valider
-        </button>
-        </div>
-      </form>
-    </div>
-    
-    
-    </>
-  );
+            <div className="Inscription">
+                <h1>Inscription</h1>
+                <form>
+                    <input
+                    type="text"
+                    name="pseudo"
+                    placeholder="Entrez votre pseudonyme"
+                    />
+                    <div>
+                    <input
+                    type="text"
+                    name="email"
+                    placeholder="Entrez votre email"
+                    />
+                    <span>Message d'erreur *</span>
+                    </div>
+                    <br/>
+                    <div> 
+                        <input
+                        type="text"
+                        name="password"
+                        placeholder="Entrez votre mdp"
+                    />
+                    <span>Message d'erreur *</span>
+
+                    <button type="submit">
+                    Valider
+                    </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 };
         
 export default Inscription;
