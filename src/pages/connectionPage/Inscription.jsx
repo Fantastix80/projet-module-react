94d577f2a0f43a940224f3a4aa817adce7e2bf40
Fantastix80 <jean.vanwittenberge@gmail.com>
2023-12-14 12:13:03 +0100

    Début modif formulaire

--- src/pages/connectionPage/Inscription.jsx
+++ src/pages/connectionPage/Inscription.jsx
@@ -1,38 +1,23 @@
+import { useContext } from 'react';
 import '../../assets/css/inscription.css'
+import { UserContext } from '../../context/UserProvider';
 
 
 const Inscription = () => {
+    const {user, setUser} = useContext(UserContext);
+
     return (
         <>
             <div className="Inscription">
                 <h1>Inscription</h1>
                 <form>
-                    <input
-                    type="text"
-                    name="pseudo"
-                    placeholder="Entrez votre pseudonyme"
-                    />
-                    <div>
-                    <input
-                    type="text"
-                    name="email"
-                    placeholder="Entrez votre email"
-                    />
+                    <input type="text" name="pseudo" placeholder="Entrez votre pseudonyme" />
+                    <input type="text" name="email" placeholder="Entrez votre email" />
                     <span>Message d'erreur *</span>
-                    </div>
                     <br/>
-                    <div> 
-                        <input
-                        type="text"
-                        name="password"
-                        placeholder="Entrez votre mdp"
-                    />
+                    <input type="text" name="password" placeholder="Entrez votre mdp"/>
                     <span>Message d'erreur *</span>
-
-                    <button type="submit">
-                    Valider
-                    </button>
-                    </div>
+                    <button type="submit">Valider</button>
                 </form>
             </div>
         </>
